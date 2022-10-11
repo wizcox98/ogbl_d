98e88f70678d5d36ef769c02992a64355e776069
WizcoxYu <55198778+WizcoxYu@users.noreply.github.com>
2022-10-11 16:16:09 +0800

    Add files via upload

---
+++ ddi.sh
@@ -0,0 +1,21 @@
+python -u main.py \
+    --dataset ogbl-ddi \
+    --model agdn \
+    --eval-steps 1 \
+    --log-steps 100 \
+    --epochs 600 \
+    --negative-sampler global \
+    --eval-metric hits \
+    --lr 0.001 \
+    --K 3 \
+    --hop-norm \
+    --n-layers 2 \
+    --n-hidden 512 \
+    --n-heads 1 \
+    --dropout 0.4 \
+    --attn-drop 0. \
+    --input-drop 0. \
+    --diffusion-drop 0. \
+    --loss-func AUC \
+    --n-neg 3 \
+    --bn
